293d17f4d3734bc7a9d2ff9b0c0802996b17838e
dundun <costes.duncan@gmail.com>
2025-05-19 00:32:10 +0200

    ajout boutton disconnect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
+import axios from "axios";
+
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  const disconnecting = () => {
+    axios.get("http://127.0.0.1:8080/logout",{ withCredentials: true}).then((responce) => {
+      console.log(responce);
+    });
+    navigate('/')
+  }
+
   return (
     <nav className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
@@ -30,6 +42,11 @@ export default function NavBar() {
           >
             Utilisateurs
           </Link>
+          <div 
+          onClick={() => { disconnecting() }}
+          className="text-gray-700 hover:text-blue-600 font-medium transition duration-200">
+            disconnect
+          </div>
         </div>
       </div>
     </nav>
